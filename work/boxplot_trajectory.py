a4df875cde6cdfcc9592978b94521501111bd470
H1r0ak1Y0sh10ka <yoshioka-hiroaki-sn@ynu.ac.jp>
2025-06-02 00:39:20 +0900

    Update using ymal configuration in boxplot_trajectory.py

--- work/boxplot_trajectory.py
+++ work/boxplot_trajectory.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import yaml
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src/'))
 
 import logging
@@ -14,39 +15,39 @@ import pyhurricane.util as util
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s %(levelname)s %(name)s %(lineno)d] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
+##### Read yaml file
+with open('run_trajectory_conf.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+
+model_name       = config['model_name']    # Model name, you can set any name.
+tc_name          = config['tc_name']       # Typhoon name, you can set any name.
+ex_name          = config['ex_name']       # Experiment name, you can set any name.
+plot_kind        = config['plot_kind']     # 'boxplot' or 'scatter'
+param            = config['param']         # 'distance' or 'qv', 'rh' ###or hgt
+
+ex_kind          = config["ex_kind"]       # Plot experiments, "individual" or "ensemble"
+draw_height      = config["draw_height"]   # Number of height layers to be drawn
+
+prefix           = config["prefix"]
+radix            = config["radix"]
+suffix           = config["suffix"]
+
+init_data_type        = config["init_data_type"]        # Kinds of setting parcels, 'square' or 'round' but 'square' is not implemented yet.
+radius                = config["radius"]                #  Setted parcels in distance from the center of typhoon [m].
+track_type            = config["track_type"]            # Kinds of trajectory, 'forward' or 'back' but 'back' is not implemented yet.
+track_init_time       = config["track_init_time"]       # Trajectory start time in analysis [hour]
+track_base_time_start = config["track_base_time_start"] # Trajectry start time in calculation [hour]
+track_time            = config["track_time"]            # Trajectry time in calculation [hour]
+
+input_model_data_dir        = config["input_model_data_dir"]        #Input data and dirctory
+input_model_name_suffix     = config["input_model_name_suffix"]
+input_trajectry_data_dir    = config["input_trajectry_data_dir"]
+
+case_names            = config["case_names"]
+legends               = config["legends"]
+
+
 ########## Main part
-
-##### Configuration
-model_name       = "scale"       # Model name, you can set any name.
-tc_name          = "faxai"       # Typhoon name, you can set any name.
-ex_name          = "cd_radius"   # Experiment name, you can set any name.
-plot_kind        = 'boxplot'     # 'boxplot' or 'scatter'
-param            = 'qv'          # 'distance' or 'qv', 'rh' ###or hgt
-
-ex_kind          = "individual"   # Plot experiments, "individual" or "ensemble"
-draw_height      = 1             # Number of height layers to be drawn
-
-prefix           = ""
-radix            = "_trajectory_timeseries_"
-suffix           = ""
-
-#### Input data and dirctory
-init_data_type        = 'round'     # Kinds of setting parcels, 'square' or 'round' but 'square' is not implemented yet.
-radius                = 250000      #  Setted parcels in distance from the center of typhoon [m].
-track_type            = 'forward'   # Kinds of trajectory, 'forward' or 'back' but 'back' is not implemented yet.
-track_init_time       = 0           # Trajectory start time in analysis [hour]
-track_base_time_start = 18          # Trajectry start time in calculation [hour]
-track_time            = 24          # Trajectry time in calculation [hour]
-
-# Please set the directory and file name of the model data and trajectory data in your environment.
-input_model_data_dir        = "/mnt/data1/model/scale/faxai/fnl/flow/"
-input_model_name_suffix     = "pe000000.nc"
-input_trajectry_data_dir    = "/home/yoshioka/research/analysis/python/scale/trajectory/trajectory_data_360/"
-
-#case_names            = ['CTL','Cd1730','Cd1530','Cd1330','Cd1130','Cd1030'] #case_names = ('CTL','25km','50km','75km','100km','150km','200km')
-#legends               = ['CTL','R025km','R050km','R100km','R150km','R200km'] #legends = case_names
-case_names = ['CTL']
-legends    = ['CTL']
 
 #### Output data and dirctory
 
@@ -58,8 +59,8 @@ if "timeseries" in prefix or "timeseries" in radix or "timeseries" in suffix or 
     save_prefix      = model_name+'-'+tc_name+"-"+ex_name+"-"+str(plot_kind[0].upper())+radix+param+"_"+ex_kind
 
 output_fig_name  = save_prefix
-output_fig_type  = 'png'
-dpi              = 350
+output_fig_type  = config["output_fig_type"]
+dpi              = config["dpi"]
 
 #### Add configuration
 util.create_dir(output_dir_path)
